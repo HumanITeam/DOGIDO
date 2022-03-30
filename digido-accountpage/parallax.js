121a49b317eb54ff343a841c8144bbc0ed6b7c6b
JEaLiFe <83034287+jealife@users.noreply.github.com>
2022-03-30 20:01:56 +0200

    Delete parallax.js

--- digido-accountpage/parallax.js
+++
@@ -1,9 +0,0 @@
-function parallax(element, distance, speed){
-    const item = document.querySelector(element);
-
-    item.style.transform
-}
-
-window.addEventListener('scroll', function(){
-    parallax('header',window,1);
-})
