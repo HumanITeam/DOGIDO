e8cc1fedc89af2a1c17267aa3183d3e4cb0c4ba6
JEaLiFe <83034287+jealife@users.noreply.github.com>
2022-03-31 14:39:09 +0200

    Update marketplace.inc.php

--- en/php/inc.php/marketplace.inc.php
+++ en/php/inc.php/marketplace.inc.php
@@ -8,19 +8,17 @@
 					foreach ($arr as &$item) {
 						$item++;
 						echo'<div class="item">
-
-						<img src="PNG/donor.png" alt="">
+						<div id="podcut_image" class="podcut_image"><img src="PNG/donor.png" alt=""></div>
 						<div class="subtitle">
 							<h4>Product name</h4>
-						<h5>item type</h5>
-						<h6>Price/Donation</h6>
+							<h5>item type</h5>
+							<h6>Price/Donation</h6>
 						</div>
 						<div class="user">
-							<div class="user-profil"> <i class="fa-solid fa-user-circle"></i> </div>
-							<a href="#" class="acount_product" style="color: #f5aa18;">user_name</a>
-							<a href="index.php?ref=product&lang='.$lang.'">item'.$item.'</a>
+							<div class="user-profil"> <i class="fa-solid fa-user-circle"></i> 
 						</div>
-
+                                		<a href="#" class="acount_product" style="color: #f5aa18;">user_name</a>
+							<a href="index.php?ref=product&lang='.$lang.'">item'.$item.'</a>
 						</div>';
 					} 
 					?>
