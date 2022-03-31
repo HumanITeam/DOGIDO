a1b1ad477de0a70033949f3297ea0e8693b5da3b
JEaLiFe <83034287+jealife@users.noreply.github.com>
2022-03-31 12:45:38 +0200

    Update main.php

--- en/php/main.php
+++ en/php/main.php
@@ -1,6 +1,40 @@
 <div class="shape1"></div>
 
     <div class="shape2"></div>
+
+    <header class="top-page">
+        <nav class="navbar-container">
+            <div class="con-logo"id="navbar__logo" >
+             <a id="title" href="#top-page"> <p>DigiDo</p></a>
+            </div>
+            <div class="navbar__toggle" id="mobile-menu">
+                <a class="bar"></a>
+                <a class="bar"></a>
+                <a class="bar"></a>
+            </div>
+            <ul class="navbar__menu">
+                <li class="navbar__item"> <a href="index.html"  class="navbar__link" id="active_page" >Home</a> </li>
+                <li class="navbar__item"> <a href="#marketplace" onclick="" class="navbar__link">Marketplace</a> </li>
+                
+                <li class="navbar__item"> <a href="contact.html" class="navbar__link" class="navbar__link">Contacts</a> </li>
+                <li class="navbar__item"> <a href=""  class="navbar__link">About us</a> </li>
+           </ul>
+           <div class="cotaier">
+            <div class="search-bar ">
+                <input class="search_txt"  type="text" placeholder="search"> 
+                <a href="" class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></a>
+            </div>
+           <div class="btns">
+                <li class="btn-sign_in"><a href="">sign in </a></li>
+                <li class="btn-suscribe"><a href="">Get started </a></li>
+            </div>
+           </div>
+        </nav> 
+        <img id="sec1" src="images/shape (3).png" alt="">
+        <img id="sec2" src="images/shape (3).png" alt="">
+        <img id="sec3" src="images/shape (3).png" alt="">
+        <img id="sec3_" src="images/shape (3).png" alt="">
+    </header>
 <?php
 include($lang."/php/inc.php/presentation.inc.php");
 include($lang."/php/inc.php/marketplace.inc.php");
