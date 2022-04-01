f12d81c7e34a169b3e1256d61d564eeed76fe18a
Konnattzu <hugo.flhs@outlook.fr>
2022-04-01 10:14:12 +0200

    Update connect.inc.php

--- en/php/inc.php/connect.inc.php
+++ en/php/inc.php/connect.inc.php
@@ -6,7 +6,7 @@
 
         <div class="login_elements">
             <div class="name">
-                <input type="email" name="email" required>
+                <input type="mail" name="email" required>
                 <label>Email</label>
               </div>
               <a class="line1">You can use letters, numbers & periods</a>
