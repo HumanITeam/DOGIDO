378c5eda166526540595d10469bd9fa4625cfb0f
Konnattzu <hugo.flhs@outlook.fr>
2022-04-01 10:27:10 +0200

    video

--- en/php/inc.php/presentation.inc.php
+++ en/php/inc.php/presentation.inc.php
@@ -1,6 +1,6 @@
 <section class="section1">   
         <div class="video_prsentation">
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/TR1Jymo2O4w" title="YouTube video player" frameborder="0" 
+            <iframe width="100%" height="100%" src="images/digidoPromobyFurkanv3.mp4" title="YouTube video player" frameborder="0" 
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" autoplay loop allowfullscreen>
 			</iframe>
         </div>
