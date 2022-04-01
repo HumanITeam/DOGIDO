81affc10b33b8ba6f93ade04c0d76dd7681f2b26
JEaLiFe <83034287+jealife@users.noreply.github.com>
2022-04-01 06:10:25 +0200

    Update footer.php

--- en/footer.php
+++ en/footer.php
@@ -1,5 +1,46 @@
 
     <footer class="footer">
+	    <div class="footer-contact">
+            <div class="first-items">
+                <a href="#">Contact Us</a>
+                <br>
+                <a href="#">About us</a>          
+            </div>
+        <div class="email">
+            <div class="footer-email">
+               <input class="email" type="email" placeholder="Enter your mail" required>
+            </div>
+            <div class="boutton">
+               <input class="subscribe_bottom" type="submit" value="Subscribe">
+            </div>
+        </div>
+         
+    </div> 
+    <div class="last-bloc">
+        <div class="social-media">
+            <a href="https://www.linkedin.com/mwlite/in/digido-iobc-435068236"><i class="fa-brands fa-linkedin fa-2x"></i></a>        
+            <a href="https://www.instagram.com/digido.iobc/"><i class="fa-brands fa-instagram fa-2x"  ></i></a>
+        </div>
+        <div class="company">
+                <span>2022 DIGIDO,Inc.</span>
+            </div>
+        <div class="privacy-policy">
+            
+            <div class="footer-link">
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Privacy policy</a>
+                <a href="#">Security</a>
+            </div>
+            
+        </div>
+        <div class="legal">
+            <div class="check">
+                <input class="checkbox" type="checkbox">
+            </div>
+            <p>By checking this box you accept the <a class="legal_" href="">Legal conditions of use</a> .</p>
+        
+        </div>
+    </div> 
 		<?php
 		echo'
 		<a href="index.php?ref=about-us&lang='.$lang.'">About us</a>
